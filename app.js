07add98b9e2668e9482c268948b07427d3d4b7be
Jovan R <jocoraos@yahoo.com>
2021-02-27 18:49:09 +0100

    added mongoose

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-const errorController = require("./controllers/error");
-
 app.set("view engine", "ejs");
+
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
@@ -16,9 +19,40 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById("602a6eb1d395c555e8107ff5")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect(
+    "mongodb+srv://JoBaHP:3weTuY5pzGgALEU@cluster0.7fnue.mongodb.net/WebShop?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Jovan",
+          email: "jovan@yahoo.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
